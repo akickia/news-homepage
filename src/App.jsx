62170e27904299f314a15c4511c1a5c697e85f9f
tshepiso <tshepi.tech@gmail.com>
2023-06-14 15:54:36 +0200

    chore: ESLint rules and prettier config done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
-
+  const tryThis = 10;
 
   return (
-    <div className="App">
-      
-    <h1>Project news homepage</h1>
+    <div className='App'>
+      <h1>Project news homepage</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
