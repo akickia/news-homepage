ac9c4daebfd5c44ae34680fb3437a358c43ee991
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-06-19 12:45:53 +0200

    feat: new component created and styled

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Header from './Components/Header';
 
 import './App.css';
 import HeroSection from './Components/HeroSection';
+import NewContainer from './Components/NewContainer';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-
       <HeroSection />
+      <NewContainer />
     </div>
   );
 }
---
+++ src/Components/NewCard.jsx
@@ -0,0 +1,11 @@
+
+
+export default function NewCard({text, title}) {
+  return (
+    <section>
+      <h3>{title}</h3>
+      <p>{text}</p>
+      <hr></hr>
+    </section>
+  )
+}
